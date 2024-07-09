5fe5937afb52527887d56a1e6ad8e32a5a54d31e
Abner Leandro Gonçalves <99682910+AbN13@users.noreply.github.com>
2024-07-09 18:45:50 -0300

    Update auxiliar.py

--- auxiliar.py
+++ auxiliar.py
@@ -4,9 +4,6 @@ import time
 time.sleep(4)
 print(pyautogui.position())
 
-# Passo 2 - Fazer Login
-#pyautogui.click(x=830, y=464)
-#pyautogui.hotkey("ctrl", "a")
-#pyautogui.write("work.abn96@gmail.com")
+
 
 
